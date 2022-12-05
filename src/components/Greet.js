6e641e58b5f11dd0053264e41dc3896bbfca8e05
KONG LY <paly.kong@z1fsc.com>
2022-12-05 10:05:35 +0700

    ReactJs

---
+++ src/components/Greet.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// function Greet() {
+//     return <h1>Hello World</h1>
+// }
+
+// ES 6 arrow function
+export const Greet = () => <h1>Hello World</h1>
+
+// export default Greet;
