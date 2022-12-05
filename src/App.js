f7f25400009eb03502f0484ab6213ee0b02c9a48
KONG LY <paly.kong@z1fsc.com>
2022-12-05 14:57:13 +0700

    setState

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import Greet from './components/Greet'
 import Welcome from './components/Welcome'
 import Hello from './components/Hello';
 import Massage from './components/Message'
+import Counter from './components/Counter';
 
 class App extends Component {
   render(){
     return (
       <div className='App'>
-        <Massage />
+        <Counter />
         {/* <Greet name="Bruce" heroName="Batman" >
           <p>This is children props</p>
         </Greet>
