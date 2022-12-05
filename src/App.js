f3bb866733ef52bcd3a6cf684312745d7ed39265
KONG LY <paly.kong@z1fsc.com>
2022-12-05 16:39:32 +0700

    Binding Event Hendlers

--- src/App.js
+++ src/App.js
@@ -8,13 +8,15 @@ import Massage from './components/Message'
 import Counter from './components/Counter'
 import FunctionClick from './components/FunctionClick'
 import ClassClick from './components/ClassClick'
+import EventBind from './components/EventBind'
 
 class App extends Component {
   render(){
     return (
       <div className='App'>
-        <FunctionClick />
-        <ClassClick />
+        <EventBind />
+        {/* <FunctionClick />
+        <ClassClick /> */}
         {/* <Greet name="Bruce" heroName="Batman" >
           <p>This is children props</p>
         </Greet>
