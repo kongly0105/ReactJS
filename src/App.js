6b9a8693bd17820cdbebd2e3068f85bde3c280ec
KONG LY <paly.kong@z1fsc.com>
2022-12-07 15:18:47 +0700

    Portals

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import FRParentInput from './components/FRParentInput';
+import PortalDemo from './components/PortalDemo';
+
 
 class App extends Component {
   render(){
     return (
       <div className='App'>
-        <FRParentInput />
+        <PortalDemo />
       </div>
     );
   }
