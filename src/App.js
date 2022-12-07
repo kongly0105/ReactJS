06334bbc66179a76458d0a70d22f6f8bb773ade7
KONG LY <paly.kong@z1fsc.com>
2022-12-07 13:45:10 +0700

    Refs

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 // import PureComp from './components/PureComp'
 import ParentComp from './components/ParentComp';
+import RefsDemo from './components/RefsDemo';
 class App extends Component {
   render(){
     return (
       <div className='App'>
-        <ParentComp />
+        <RefsDemo />
+        {/* // <ParentComp /> */}
         {/* <PureComp /> */}
       </div>
     );
