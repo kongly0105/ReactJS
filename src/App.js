6ff669052041b408abed375eebd55d6a3b940794
KONG LY <paly.kong@z1fsc.com>
2022-12-07 08:57:43 +0700

    Component Mounting Lifcycle

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Form from './components/Form'
+import LifecycleA from './components/LifecycleA';
 class App extends Component {
   render(){
     return (
       <div className='App'>
-        <Form />
+        <LifecycleA />
+        {/* <Form /> */}
       </div>
     );
   }
---
+++ src/components/LifecycleB.js
@@ -0,0 +1,26 @@
+import React, {Component} from "react";
+
+class LifecycleB extends Component {
+    constructor (props) {
+        super(props)
+
+        this.state = {
+            name: "Kong Ly"
+        }
+        console.log("LifecycleB constructor");
+    }
+
+    static getDerivedStateFromProps(props, state){
+        console.log('LifecycleB getDerivedStateFromProps');
+        return null
+    }
+    componentDidMount() {
+        console.log('LifecycleB componentDidMount');
+    }
+    render() {
+        console.log('LifecycleB render');
+        return <div>Lifecycle B</div>
+    }
+}
+
+export default LifecycleB
