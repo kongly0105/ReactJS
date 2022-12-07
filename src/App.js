9ed513f67af88b7b9d8f136e1b34f2e84719c938
KONG LY <paly.kong@z1fsc.com>
2022-12-07 14:44:24 +0700

    Forwarding Refs

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-// import PureComp from './components/PureComp'
-import ParentComp from './components/ParentComp';
-import RefsDemo from './components/RefsDemo';
-import FocusInput from './components/FocusInput';
+import FRParentInput from './components/FRParentInput';
+
 class App extends Component {
   render(){
     return (
       <div className='App'>
-        <FocusInput />
-        {/* <RefsDemo /> */}
-        {/* // <ParentComp /> */}
-        {/* <PureComp /> */}
+        <FRParentInput />
       </div>
     );
   }
