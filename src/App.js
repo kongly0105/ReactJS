2dde6e156a5086c76425bc575e7037578faddf21
KONG LY <paly.kong@z1fsc.com>
2022-12-07 11:12:29 +0700

    Pure Component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Form from './components/Form'
-import LifecycleA from './components/LifecycleA';
-import FragmentDemo from './components/FragmentDemo'
-import Table from './components/Table';
+// import PureComp from './components/PureComp'
+import ParentComp from './components/ParentComp';
 class App extends Component {
   render(){
     return (
       <div className='App'>
-        <Table />
-        {/* <FragmentDemo /> */}
-        {/* <LifecycleA /> */}
-        {/* <Form /> */}
+        <ParentComp />
+        {/* <PureComp /> */}
       </div>
     );
   }
---
+++ src/components/ParentComp.js
@@ -0,0 +1,35 @@
+import React, {Component, PureComponent} from "react";
+import RegComp from './RegComp'
+import PureComp from "./PureComp";
+
+class ParentComp extends Component {
+
+    constructor(props){
+        super(props)
+
+        this.state = {
+            name: 'Vishwas'
+        }
+    }
+
+    componentDidMount() {
+        setInterval(() => {
+            this.setState({
+                name: 'Vishwas'
+            })
+        }, 2000)
+    }
+
+    render() {
+        console.log('***Parent Component****');
+        return (
+            <div>
+                Parent Component
+                <RegComp name = {this.state.name} />
+                <PureComp name = {this.state.name} />
+            </div>
+        )
+    }
+}
+
+export default ParentComp
