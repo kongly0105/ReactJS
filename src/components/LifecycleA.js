5b8b7be7a8bbad4c85376551757cd048e0b6f936
KONG LY <paly.kong@z1fsc.com>
2022-12-07 09:26:42 +0700

    Component Updating Lifecycle

--- src/components/LifecycleA.js
+++ src/components/LifecycleA.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import LifecycleB from "./LifecycleB";
 
 class LifecycleA extends Component {
+
     constructor (props) {
         super(props)
 
@@ -15,14 +16,37 @@ class LifecycleA extends Component {
         console.log('LifecycleA getDerivedStateFromProps');
         return null
     }
+
     componentDidMount() {
         console.log('LifecycleA componentDidMount');
     }
+
+    shouldComponentUpdate(){
+        console.log('LifecycleA shouldComponentUpdate');
+        return true
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState){
+        console.log('LifecycleA getSnapshotBeforeUpdate');
+        return null
+    }
+
+    componentDidUpdate() {
+        console.log('LifecyclA componentDidUpdate');
+    }
+
+    changeState = () => {
+        this.setState({
+            name: 'Codevolution'
+        })
+    }
+
     render() {
         // console.log('LifecycleA render');
         return (
             <div>
                 <div>Lifecycle A</div>
+                <button onClick={this.changeState}>Change State</button>
                 <LifecycleB />
             </div>
         )
--- src/components/LifecycleB.js
+++ src/components/LifecycleB.js
@@ -14,11 +14,27 @@ class LifecycleB extends Component {
         console.log('LifecycleB getDerivedStateFromProps');
         return null
     }
+
     componentDidMount() {
         console.log('LifecycleB componentDidMount');
     }
+
+    shouldComponentUpdate(){
+        console.log('LifecycleB shouldComponentUpdate');
+        return true
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState){
+        console.log('LifecycleB getSnapshotBeforeUpdate');
+        return null
+    }
+
+    componentDidUpdate() {
+        console.log('LifecycleB componentDidUpdate');
+    }
+
     render() {
-        console.log('LifecycleB render');
+        // console.log('LifecycleB render');
         return <div>Lifecycle B</div>
     }
 }
