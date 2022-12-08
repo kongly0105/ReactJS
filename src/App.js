a763e5ce954041ce01e20585fd6f2569c4e8b289
KONG LY <paly.kong@z1fsc.com>
2022-12-08 13:38:28 +0700

    Render Props (Part 1)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ClickCounter from './components/ClickCounter'
-import HoverCounter from './components/HoverCounter';
+import ClickCounterTwo from './components/ClickCounterTwo'
+import HoverCounterTwo from './components/HoverCounterTwo'
 class App extends Component {
   render(){
     return (
       <div className='App'>
-        <ClickCounter name= 'Vishwas' />
-        <HoverCounter />
+        <ClickCounterTwo />
+        <HoverCounterTwo />
       </div>
     );
   }
