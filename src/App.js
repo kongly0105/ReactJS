a4ace71551ec60f004d730610e3f9c2313aa18cc
KONG LY <paly.kong@z1fsc.com>
2022-12-08 16:19:26 +0700

    Context (Part 1)

--- src/App.js
+++ src/App.js
@@ -1,27 +1,18 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ClickCounterTwo from './components/ClickCounterTwo';
-import HoverCounterTwo from './components/HoverCounterTwo';
-// import User from './components/User'
-import Counter from './components/Counter';
+import ComponentC from './components/ComponentC'
+import { UserProvider } from './components/UserContext';
+
 class App extends Component {
   render(){
     return (
       <div className='App'>
-        <Counter>
-          {(count, incrementCount) => (
-            <ClickCounterTwo count={count} incrementCount={incrementCount} />
-          )}
-        </Counter>
-        <Counter>
-          {(count, incrementCount) => (
-            <HoverCounterTwo count={count} incrementCount={incrementCount} />
-          )}
-        </Counter>
-        {/*<User render={ (isLoggedIn) => isLoggedIn ? 'Vishwas' : 'Guest'} />*/}
+        <UserProvider value='Vishwas'>
+          <ComponentC />
+        </UserProvider>
       </div>
-    );
+    )
   }
 }
 
